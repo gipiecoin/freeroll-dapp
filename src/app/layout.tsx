20696dce7ee1870caa2748559c0b9cd682e61f92
gipiecoin <gipiecoin@gmail.com>
2025-06-19 08:56:48 +0700

    Re-upload freeroll project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next';
      import { WalletProvider } from "./context/walletContext";
 
      export const metadata: Metadata = {
-       title: 'GipieCoin - Freeroll, Claim & Win',
+       title: 'GipieCoin - Freeroll, Stake & Win',
        description: 'Win free crypto every hour with the GipieCoin Freeroll, stake your tokens, and earn passive rewards!',
        icons: {
          icon: '/favicon.ico', // Hanya gunakan favicon.ico untuk menguji
